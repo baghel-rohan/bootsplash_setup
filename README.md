8cab908d09d988c78a17dc3f860caae8444a8d99
Rohan <rohan.1@thoughts2binary.com>
2023-12-15 16:33:01 +0530

    feat: add script to make bootsplash logo for all flavours

---
+++ README.md
@@ -0,0 +1,112 @@
+# React Native Boot-splash Setup
+
+---
+
+## Basic Setup
+
+- Add react-native-bootsplash@4.7.5 package in your project.
+- Assets for the splash screen, provide the images for all the flavours you want to create splash screen for in format.
+- The images have to be added in projectRoot/assets/{flavourName}.png (Name of the image has to be same as product dimension's or name of the folder in android/app/src)
+- You should check for all options of script by using -h flag before running the script.
+
+## Run the script
+
+- After the above setup run the script (arguments if needed, preferred default arguments).
+
+### iOS
+
+- Setup storyboard of the flavour however you want.
+
+- Edit the `ios/YourProjectName/AppDelegate.m(m)` file:
+
+```obj-c
+#import "AppDelegate.h"
+#import "RNBootSplash.h" // <- add the header import
+
+// …
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  // …
+
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+
+  // from here
+  NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+
+  NSDictionary *infoDict = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+
+  NSString *flavorStoryboardName = infoDict[@"UILaunchStoryboardName"];
+
+  UIView *rootView = [super createRootViewWithBridge:bridge
+                                            moduleName:@"theftpatrol"
+                                             initProps:nil]; // <- Add only if rootView is not already present in the method
+  [RNBootSplash initWithStoryboard:flavorStoryboardName rootView:rootView]; // <- initialization using the storyboard file name
+
+  return YES;
+}
+```
+
+### Android
+
+_⚠️ On Android >= 12, the splash screen will not appear if you start your app from the terminal / Android Studio. To see it, kill your app and restart it in normal conditions (tap on your app icon in the app launcher)._
+
+---
+
+1. As this library only support Android 6+, you probably have to edit your `android/build.gradle` file:
+
+```gradle
+buildscript {
+  ext {
+    buildToolsVersion = "31.0.0"
+    minSdkVersion = 23 // <- AndroidX splashscreen has basic support for 21 (only the background color), so 23 is best
+    compileSdkVersion = 31 // <- set at least 31
+    targetSdkVersion = 31 // <- set at least 31
+
+    // …
+```
+
+2. Then edit your `android/app/build.gradle` file:
+
+```gradle
+dependencies {
+  // …
+
+  implementation "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"
+  implementation "androidx.core:core-splashscreen:1.0.0" // Add this line
+
+  // …
+```
+
+3. Edit your `android/app/src/main/res/values/styles.xml` file:
+
+```xml
+<resources>
+
+  <style name="AppTheme" parent="Theme.AppCompat.DayNight.NoActionBar">
+      <!-- Your base theme customization -->
+  </style>
+
+  <!-- BootTheme should inherit from Theme.SplashScreen -->
+  <style name="BootTheme" parent="Theme.SplashScreen">
+    <item name="windowSplashScreenBackground">@color/bootsplash_background</item>
+    <item name="windowSplashScreenAnimatedIcon">@mipmap/bootsplash_logo</item>
+    <item name="postSplashScreenTheme">@style/AppTheme</item>
+  </style>
+
+</resources>
+```
+
+#### Usage
+
+```js
+import RNBootSplash from "react-native-bootsplash"
+
+RNBootSplash.hide() // immediate
+RNBootSplash.hide({ fade: true }) // fade with 220ms default duration
+RNBootSplash.hide({ fade: true, duration: 500 }) // fade with custom duration
+```
--- index.js
+++ index.js
@@ -1,46 +1,75 @@
-const fs = require('fs')
-const { execSync } = require('child_process')
-const path = require('path')
+#!/usr/bin/env node
 
-const bootsplashPackage = 'react-native-bootsplash'
-const bootsplashVersion = '4.7.5'
+const fs = require("fs")
+const { execSync } = require("child_process")
+const path = require("path")
+const yargs = require("yargs")
+const xml2js = require("xml2js")
 
-// Step 1: Check if react-native-bootsplash is installed
-const packageJsonPath = '../package.json'
-const packageJson = require(packageJsonPath)
+const argv = yargs
+  .option("projectRelativePath", {
+    alias: "p",
+    describe: "Relative path to the React Native project directory",
+    demandOption: true,
+    type: "string",
+    default: ".",
+  })
+  .option("backgroundColor", {
+    alias: "bg",
+    describe: "Background color or the splash screens of the flavours",
+    demandOption: true,
+    type: "string",
+    default: "#FFFFFF",
+  })
+  .option("logoSize", {
+    alias: "s",
+    describe: "Size of logo according to the android splashscreen guidelines",
+    demandOption: true,
+    type: "number",
+    default: 192,
+  })
+  .help()
+  .alias("help", "h").argv
 
-if (!packageJson.dependencies || !packageJson.dependencies[bootsplashPackage]) {
-  // Step 2: Install react-native-bootsplash
-  const lockFile = fs.existsSync('../yarn.lock') ? 'yarn.lock' : 'package-lock.json'
-  const installCommand = fs.existsSync('../yarn.lock') ? 'yarn add' : 'npm install'
+const projectPath = argv.projectRelativePath
+const backgroundColor = argv.backgroundColor
+const logoSize = argv.logoSize
+console.log("argv.logoSize==>", logoSize, projectPath, backgroundColor)
 
-  try {
-    execSync(`${installCommand} ${bootsplashPackage}@${bootsplashVersion}`, { stdio: 'inherit' })
-  } catch (error) {
-    console.error('Failed to install react-native-bootsplash. Exiting script.')
-    process.exit(1)
-  }
+function getDirectories(path) {
+  return fs
+    .readdirSync(path, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
 }
+const directoryPath = `${projectPath}/android/app/src`
 
-// Step 3: Wait for package installation
+let directories = []
+try {
+  directories = getDirectories(directoryPath)
+} catch (error) {
+  console.error("Error reading directory:", error)
+}
+console.log("directories==>", directories)
 
-// Step 4: Run react-native generate-bootsplash script for each flavor
-const buildGradlePath = '../android/app/build.gradle'
-const buildGradle = fs.readFileSync(buildGradlePath, 'utf-8')
+let flavors = []
 
-const flavorMatches = buildGradle.match(/productFlavors\s*{([\s\S]*?)}/)
-const flavors = flavorMatches ? flavorMatches[1].match(/\w+/g) : ['main']
+directories.forEach((item) => {
+  if (item == "androidTest" || item == "debug" || item == "main") {
+  } else {
+    flavors.push(item)
+  }
+})
 
 flavors.forEach((flavor) => {
   // Check if the corresponding PNG file exists
-  const pngPath = `./assets/${flavor}.png`
+  const pngPath = `${projectPath}/assets/${flavor}.png`
 
   if (fs.existsSync(pngPath)) {
-    const backgroundColor = '#FFFFFF' // Replace with user input
-    const command = `yarn react-native generate-bootsplash ./assets/${flavor}.png --platforms=android --flavor=${flavor} --logo-width=192 --background-color=${backgroundColor}`
+    const command = `yarn react-native generate-bootsplash ${projectPath}/assets/${flavor}.png --platforms=android,ios --flavor=${flavor} --logo-width=${logoSize} --background-color=${backgroundColor}`
 
     try {
-      execSync(command, { stdio: 'inherit' })
+      execSync(command, { stdio: "inherit" })
     } catch (error) {
       console.error(`Failed to run ${command}. Exiting script.`)
       process.exit(1)
@@ -49,69 +78,76 @@ flavors.forEach((flavor) => {
 })
 
 // Step 5: Add implementation("androidx.core:core-splashscreen:1.0.0") to dependencies in build.gradle
-const coreSplashscreenDependency = 'implementation("androidx.core:core-splashscreen:1.0.0")'
-const dependenciesIndex = buildGradle.lastIndexOf('dependencies')
-if (dependenciesIndex !== -1) {
-  const updatedBuildGradle =
-    buildGradle.slice(0, dependenciesIndex + 'dependencies'.length) +
-    `\n    ${coreSplashscreenDependency}` +
-    buildGradle.slice(dependenciesIndex + 'dependencies'.length)
+// const coreSplashscreenDependency =
+//   'implementation("androidx.core:core-splashscreen:1.0.0")'
+// const dependenciesIndex = buildGradle.lastIndexOf("dependencies")
+// if (dependenciesIndex !== -1) {
+//   const updatedBuildGradle =
+//     buildGradle.slice(0, dependenciesIndex + "dependencies".length) +
+//     `\n    ${coreSplashscreenDependency}` +
+//     buildGradle.slice(dependenciesIndex + "dependencies".length)
 
-  fs.writeFileSync(buildGradlePath, updatedBuildGradle, 'utf-8')
-}
+//   fs.writeFileSync(buildGradlePath, updatedBuildGradle, "utf-8")
+// }
 
 // Step 6: Add style tag to styles.xml
-const stylesXmlPath = '../android/app/src/main/res/values/styles.xml'
-const stylesXml = fs.readFileSync(stylesXmlPath, 'utf-8')
+const stylesXmlPath = `${projectPath}/android/app/src/main/res/values/styles.xml`
+const stylesXml = fs.readFileSync(stylesXmlPath, "utf-8")
 const bootThemeStyle = `<style name="BootTheme" parent="Theme.SplashScreen">\n\t\t<item name="windowSplashScreenBackground">@color/bootsplash_background</item>\n\t\t<item name="windowSplashScreenAnimatedIcon">@mipmap/bootsplash_logo</item>\n\t\t<item name="postSplashScreenTheme">@style/AppTheme</item>\n\t</style>`
 
 if (!stylesXml.includes(bootThemeStyle)) {
-  const resourcesTagIndex = stylesXml.indexOf('<resources>')
+  const resourcesTagIndex = stylesXml.indexOf("</resources>")
   const updatedStylesXml =
-    stylesXml.slice(0, resourcesTagIndex + '<resources>'.length) +
-    `\n    ${bootThemeStyle}\n` +
-    stylesXml.slice(resourcesTagIndex + '<resources>'.length)
+    stylesXml.slice(0, resourcesTagIndex) +
+    `\n\t${bootThemeStyle}\n` +
+    stylesXml.slice(resourcesTagIndex)
 
-  fs.writeFileSync(stylesXmlPath, updatedStylesXml, 'utf-8')
+  fs.writeFileSync(stylesXmlPath, updatedStylesXml, "utf-8")
 }
 
 // Step 7: Add android:theme="@style/BootTheme" to AndroidManifest.xml
-const manifestPath = '../android/app/src/main/AndroidManifest.xml'
-const manifest = fs.readFileSync(manifestPath, 'utf-8')
+const manifestPath = `${projectPath}/android/app/src/main/AndroidManifest.xml`
+const manifest = fs.readFileSync(manifestPath, "utf-8")
 
 const packagePathMatches = manifest.match(/package="(.+?)"/)
-const packagePath = packagePathMatches ? packagePathMatches[1].replace(/\./g, '/') : ''
+const packagePath = packagePathMatches
+  ? packagePathMatches[1].replace(/\./g, "/")
+  : ""
 
-const applicationTagIndex = manifest.indexOf('<application')
-const existingThemeMatches = manifest
-  .slice(applicationTagIndex, applicationTagIndex + 200)
-  .match(/android:theme="[^"]*"/)
-const existingTheme = existingThemeMatches ? existingThemeMatches[0] : ''
+// const applicationTagIndex = manifest.indexOf("<application")
+// const existingThemeMatches = manifest
+//   .slice(applicationTagIndex, applicationTagIndex + 200)
+//   .match(/android:theme="[^"]*"/)
+// const existingTheme = existingThemeMatches ? existingThemeMatches[0] : ""
 
-if (existingTheme) {
-  const updatedManifest =
-    manifest.slice(0, applicationTagIndex + '<application'.length) +
-    manifest
-      .slice(applicationTagIndex + '<application'.length)
-      .replace(existingTheme, 'android:theme="@style/BootTheme"')
+// if (existingTheme) {
+//   const updatedManifest =
+//     manifest.slice(0, applicationTagIndex + "<application".length) +
+//     manifest
+//       .slice(applicationTagIndex + "<application".length)
+//       .replace(existingTheme, 'android:theme="@style/BootTheme"')
 
-  fs.writeFileSync(manifestPath, updatedManifest, 'utf-8')
-}
+//   fs.writeFileSync(manifestPath, updatedManifest, "utf-8")
+// }
 
 // Step 8: Modify MainActivity.java
-const mainActivityPath = `../android/app/src/main/java/${packagePath}/MainActivity.java`
-const mainActivity = fs.readFileSync(mainActivityPath, 'utf-8')
+const mainActivityPath = `${projectPath}/android/app/src/main/java/${packagePath}/MainActivity.java`
+const mainActivity = fs.readFileSync(mainActivityPath, "utf-8")
 
-if (!mainActivity.includes('RNBootSplash.init(this);')) {
-  const importStatement = 'import android.os.Bundle;\nimport com.zoontek.rnbootsplash.RNBootSplash;'
+if (!mainActivity.includes("RNBootSplash.init(this);")) {
+  const importStatement =
+    "import android.os.Bundle;\nimport com.zoontek.rnbootsplash.RNBootSplash;"
   const onCreateMethod = `\t@Override\n\tprotected void onCreate(Bundle savedInstanceState) {\n\t\tRNBootSplash.init(this);\n\t\tsuper.onCreate(savedInstanceState);\n\t}`
 
   const updatedMainActivity =
     importStatement +
-    '\n\n' +
-    mainActivity.replace(/public class MainActivity.*{/, `public class MainActivity {\n${onCreateMethod}`)
+    "\n\n" +
+    mainActivity.replace(
+      /public class MainActivity.*{/,
+      `public class MainActivity {\n${onCreateMethod}`
+    )
 
-  fs.writeFileSync(mainActivityPath, updatedMainActivity, 'utf-8')
+  fs.writeFileSync(mainActivityPath, updatedMainActivity, "utf-8")
 }
 
-console.log('Setup completed successfully!')
+console.log("Setup completed successfully!")
